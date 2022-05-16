c1e22106d6c1f2e5e4c1ab80d09e2d09328919a9
wk81652 <101109462+wk81652@users.noreply.github.com>
2022-05-16 22:02:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,68 @@
-"""
-This file runs the Flask application we are using as an API endpoint.
-"""
+%%file app.py
 
 import pickle
 from math import log10
+
 from flask import Flask
 from flask import request
 from flask import jsonify
-from sklearn import linear_model
-from sklearn.externals import joblib
+import numpy as np
 
-# Create a flask
+class Perceptron():
+    
+    def __init__(self,eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+    
+    def fit(self, X, y):
+        self.w_ = np.zeros(1+X.shape[1])
+        self.errors_ = []
+        
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X,y):
+                update = self.eta*(target-self.predict(xi))
+                self.w_[1:] += update*xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:])+self.w_[0]
+    
+    def predict(self, X):
+        return np.where(self.net_input(X)>=0.0,1,-1)
+
+# flask
 app = Flask(__name__)
 
-# Create an API end point
 @app.route('/api/v1.0/predict', methods=['GET'])
 def get_prediction():
 
-    # sepal length
+
     sepal_length = float(request.args.get('sl'))
-    # sepal width
-    sepal_width = float(request.args.get('sw'))
-    # petal length
+
     petal_length = float(request.args.get('pl'))
-    # petal width
-    petal_width = float(request.args.get('pw'))
 
-    # The features of the observation to predict
+    
     features = [sepal_length,
-                sepal_width,
-                petal_length,
-                petal_width]
-
-    # Load pickled model file
-    model = joblib.load('model.pkl')
-
-    # Predict the class using the model
-    predicted_class = int(model.predict([features]))
-
-    # Return a json object containing the features and prediction
+                petal_length]
+    
+    print(features)
+    
+    # wczytywanie modelu 
+    with open('model.pkl',"rb") as picklefile:
+        model = pickle.load(picklefile)
+    print(model)
+    
+    # predykcja
+    predicted_class = int(model.predict(features))
+    
+    # json 
     return jsonify(features=features, predicted_class=predicted_class)
 
 if __name__ == '__main__':
-    # Run the app at 0.0.0.0:3333
-    app.run(port=3333,host='0.0.0.0')
+    app.run()
+    
+python app.py
